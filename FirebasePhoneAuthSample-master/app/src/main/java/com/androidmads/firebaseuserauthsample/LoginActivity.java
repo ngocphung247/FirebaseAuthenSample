8492242b1b7e5c71116f6e4773a0c8fa8e47267c
ngocphung247 <phungpn.hust@gmail.com>
2017-12-11 20:35:06 +0700

    commit

---
+++ FirebasePhoneAuthSample-master/app/src/main/java/com/androidmads/firebaseuserauthsample/LoginActivity.java
@@ -0,0 +1,118 @@
+package com.androidmads.firebaseuserauthsample;
+
+import android.app.ProgressDialog;
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+
+
+
+/**
+ * Created by peter on 12/10/2017.
+ */
+
+public class LoginActivity  extends AppCompatActivity{
+    private EditText etEmail;
+    private EditText etPassword;
+    private Button btnLogin;
+    private static final String TAG = "LoginActivity";
+    private static final int REQUEST_SIGNUP = 0;
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+        etPassword = (EditText)findViewById(R.id.input_password);
+        etEmail = (EditText)findViewById(R.id.input_email);
+        btnLogin = (Button)findViewById(R.id.btn_login);
+        btnLogin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                login();
+            }
+        });
+    }
+
+    public void login() {
+        if (!validate()) {
+            onLoginFailed();
+            return;
+        }
+        btnLogin.setEnabled(true);
+        final ProgressDialog progressDialog = new ProgressDialog(LoginActivity.this,
+                R.style.AppTheme_Dark_Dialog);
+        progressDialog.setIndeterminate(true);
+        progressDialog.setMessage("Authenticating...");
+        progressDialog.show();
+
+        String email = etEmail.getText().toString();
+        String password = etPassword.getText().toString();
+        // TODO: Implement your own authentication logic here.
+
+        new android.os.Handler().postDelayed(
+                new Runnable() {
+                    public void run() {
+                        // On complete call either onLoginSuccess or onLoginFailed
+                        onLoginSuccess();
+                        // onLoginFailed();
+                        progressDialog.dismiss();
+                    }
+                }, 3000);
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        if (requestCode == REQUEST_SIGNUP) {
+            if (resultCode == RESULT_OK) {
+
+                // TODO: Implement successful signup logic here
+                // By default we just finish the Activity and log them in automatically
+                this.finish();
+            }
+        }
+    }
+
+    @Override
+    public void onBackPressed() {
+        moveTaskToBack(true);
+    }
+
+    public void onLoginSuccess() {
+        btnLogin.setEnabled(true);
+        Intent intent = new Intent(this, PhoneAuthActivity.class);
+        startActivity(intent);
+    }
+
+    public void onLoginFailed() {
+        Toast.makeText(getBaseContext(),"Login Failed",Toast.LENGTH_LONG).show();
+        btnLogin.setEnabled(true);
+    }
+
+    public boolean validate() {
+        boolean valid = true;
+
+        String email = etEmail.getText().toString();
+        String password = etPassword.getText().toString();
+
+        if (email.isEmpty() || !android.util.Patterns.EMAIL_ADDRESS.matcher(email).matches()) {
+            etEmail.setError("enter a valid email address");
+            valid = false;
+        } else {
+            etEmail.setError(null);
+        }
+
+        if (password.isEmpty() || password.length() < 4 || password.length() > 10) {
+            etPassword.setError("between 4 and 10 alphanumeric characters");
+            valid = false;
+        } else {
+            etPassword.setError(null);
+        }
+
+        return valid;
+    }
+}
